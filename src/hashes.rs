c19496fe5d0ecec4f6681706597bdf771538e6cf
Dan Draper <dan@cipherstash.com>
2023-12-24 14:00:41 +1100

    Added hashes and macs (WIP)

---
+++ src/hashes.rs
@@ -0,0 +1,73 @@
+use criterion::{criterion_group, criterion_main, Criterion, Throughput, BenchmarkId};
+use sha2::{Sha256, Digest};
+use sha3::{Sha3_256, Shake128, Shake256, TurboShake256, TurboShake128, TurboShake128Core, TurboShake256Core};
+use sha3::digest::{ExtendableOutput, Update};
+use tiny_keccak::{KangarooTwelve, Xof, IntoXof, Hasher};
+
+const KB: usize = 1024;
+type Benchmarker = Criterion;
+
+fn bench(c: &mut Benchmarker) {
+    let mut group = c.benchmark_group("hashes");
+
+    for size in &[10, 64, 128, 512, KB, 2 * KB, 4 * KB] {
+        let buf = vec![0u8; *size];
+        let mut out = [0u8; 32];
+
+        group.throughput(Throughput::Bytes(*size as u64));
+
+        group.bench_with_input(BenchmarkId::new("rust-crypto-sha2-256", size), buf.as_ref(), |b, input: &[u8]| {
+            b.iter(|| {
+                Sha256::digest(input);
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("rust-crypto-sha3-256", size), buf.as_ref(), |b, input: &[u8]| {
+            b.iter(|| {
+                Sha3_256::digest(&input)
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("rust-crypto-shake-128", size), buf.as_ref(), |b, input: &[u8]| {
+            b.iter(|| { Shake128::digest_xof(input, &mut out) })
+        });
+        group.bench_with_input(BenchmarkId::new("rust-crypto-shake-256", size), buf.as_ref(), |b, input: &[u8]| {
+            b.iter(|| Shake256::digest_xof(input, &mut out))
+        });
+        // TODO: Create a 256 group
+        group.bench_with_input(BenchmarkId::new("rust-crypto-turboshake-128", size), buf.as_ref(), |b, input: &[u8]| {
+            b.iter(|| {
+                let mut x = TurboShake128::from_core(TurboShake128Core::new(1));
+                x.update(input);
+                x.finalize_xof_into(&mut out);
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("rust-crypto-turboshake-256", size), buf.as_ref(), |b, input: &[u8]| {
+            b.iter(|| {
+                let mut x = TurboShake256::from_core(TurboShake256Core::new(1));
+                x.update(input);
+                x.finalize_xof_into(&mut out);
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("tiny-keccak-kangaroo-12", size), buf.as_ref(), |b, input: &[u8]| {
+            b.iter(|| {
+                let mut hasher = KangarooTwelve::new(b"");
+                hasher.update(input);
+                let mut xof = hasher.into_xof();
+                xof.squeeze(&mut out[..32]);
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("blake3-256", size), buf.as_ref(), |b, input: &[u8]| {
+            b.iter(|| { blake3::hash(input); })
+        });
+        
+    }
+
+    group.finish();
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default();
+    targets = bench
+);
+
+criterion_main!(benches);
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,10 @@
-use rand::{thread_rng, RngCore};
-use ring::{aead::{self, UnboundKey, BoundKey, NonceSequence, AES_256_GCM, Nonce, Aad}, error::Unspecified};
+use aes_gcm::aes::{Aes256, Aes128};
+use cmac::{Cmac, Mac};
 
-pub const NONCE: [u8; 96 / 8] = [0u8; 96 / 8];
 
-struct IntegerNonceSequence(u64);
-impl IntegerNonceSequence {
-    const fn new() -> Self {
-        Self(0)
-    }
-}
-
-impl NonceSequence for IntegerNonceSequence {
-    fn advance(&mut self) -> Result<Nonce, Unspecified> {
-        let mut result = [0u8; aead::NONCE_LEN];
-        result[4..].copy_from_slice(&u64::to_be_bytes(self.0));
-        self.0 = self.0.checked_add(1).ok_or(Unspecified)?;
-        Ok(aead::Nonce::assume_unique_for_key(result))
-    }
-}
 
 fn main() {
-    let mut key_bytes: [u8; 32] = Default::default();
-    let mut rng = thread_rng();
-    rng.try_fill_bytes(&mut key_bytes).unwrap();
-    let ukey = UnboundKey::new(&AES_256_GCM, &key_bytes).unwrap();
-    let mut key = ring::aead::SealingKey::new(ukey, IntegerNonceSequence::new());
-    let aad = Aad::from(b"myaad");
-
-    let mut in_out = vec![0u8; 1024 + AES_256_GCM.tag_len()];
-    let tag = key.seal_in_place_separate_tag(aad, &mut in_out).unwrap();
-
-    dbg!(tag.as_ref().len());
-    dbg!(in_out.len());
+    let key: [u8; 32] = Default::default();
+    let input: [u8; 10] = Default::default();
+    let m = Cmac::<Aes128>::new_from_slice(&key[..16]).unwrap().chain_update(input).finalize();
 }
